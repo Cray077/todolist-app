09874c81444835df7dda697297049e168354de1b
Bernando <77226440+Cray077@users.noreply.github.com>
2021-08-23 13:19:02 +0700

    Add files via upload
    added open function"open another file"

--- todolist.py
+++ todolist.py
@@ -1,14 +1,20 @@
+from os import listdir
+from os.path import isfile, join
+
 class Todolist:
+    
     def __init__(self, text_str):
         self.text_str = text_str
+        file_name = "list"
     
     def add(s):
-        ls = open("list.txt", "a")
+        ls = open(file_name + ".txt", "a")
         ls.write(s + "\n")
         ls.close
         
     def read():
-        ls = open("list.txt","r")
+        name = "list"
+        ls = open(file_name + ".txt","r", encoding = "ISO-8859-1")
         l = ls.readlines()
         todolist_number = 0
         for i in l:
@@ -17,21 +23,38 @@ class Todolist:
         ls.close()
         
     def remove(num):
-        ls = open("list.txt","r")
+        ls = open(file_name + ".txt","r")
         l = ls.readlines()
         ls.close()
         print(l[num - 1])
         l.remove(l[num - 1])
-        wls = open("list.txt", "w")
+        wls = open(file_name + ".txt", "w")
         wls.writelines(l)
         wls.close()
         
+    def file_name(name):
+        global file_name
+        file_name = name
+            
     def get_list(*i):
         print(i)
 
 command = ""
+Todolist.file_name("list")
 while command != "exit":
+    mypath = "/home/cray077/project/todolist2/"
     command = input("Enter a Command: ")
+    
+    if command == "open":
+        files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+        files.remove("todolist.py")
+        f = []
+        for name in files:
+            i = name.replace('.txt','')
+            e = i.strip()
+            print(e)
+        i = input("Name of the file: ")
+        Todolist.file_name(i)
     
     if command == "add":
         e = ""
@@ -40,7 +63,6 @@ while command != "exit":
             if e != "exit":
                 Todolist.add(e)
             else:e = "exit"
-
     if command == "read":
         Todolist.read()
         
@@ -50,23 +72,20 @@ while command != "exit":
         Todolist.remove(num)
         Todolist.read()
     
-    
     if command == "remove-m":
         Todolist.read()
         i = True
         ls = []
+        q = 0        
+        ls_ = []
         while i == True:
             e = input("Number of items to remove: ")
             if e == "done":
                 i = False
-            else: ls.append(e)
-                
-        q = 0        
-        ls_ = []
+            else: ls.append(int(e))    
         for e in ls:
             num = int(e)
             ls_.append(num)
-            
         ls_1 = sorted(ls_)
         for item in ls_1:
             num = int(item)
@@ -74,28 +93,5 @@ while command != "exit":
             Todolist.remove(num)
             q += 1
             
-        
-        
     if command == "iop":
-        Todolist.read()
-        i = True
-        ls = []
-        while i == True:
-            e = input("Number of items to remove: ")
-            if e == "done":
-                i = False
-            else: ls.append(e)
-                
-        q = 0        
-        ls_ = []
-        for e in ls:
-            num = int(e)
-            ls_.append(num)
-        
-        ls_1 = sorted(ls_)
-        #print(ls)
-        print(ls_1)
-        
-        
-        
-        
+        pass
